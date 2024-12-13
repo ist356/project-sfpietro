f2bfd3a473ca538fa9d9cb707db8d60111d8a420
sfpietro <sfpietro@syr.edu>
2024-12-13 00:53:51 -0500

    function outline

---
+++ code/functions.py
@@ -0,0 +1,28 @@
+import datetime as dt
+import requests
+import pandas as pd
+import plotly.express as px
+
+def get_birth_chart_data(data, time, location):
+    '''
+    Get birth chart data from API based on user inmput
+    '''
+    pass
+
+def process_birth_chart(raw_data):
+    '''
+    process data to get each placement
+    '''
+    pass
+
+def get_sign(chart):
+    '''
+    find star sign from birth chart data
+    '''
+    pass
+
+def get_horoscope_data(sign):
+    '''
+    fetch daily horoscope for sign
+    '''
+    pass
